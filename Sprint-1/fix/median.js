60693d1c5780b979f57beb020233b6897db12bcd
loveth900 <ojay_love@yahoo.com>
2024-12-15 06:21:30 +0000

    the median was implemented

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -9,3 +9,20 @@ function calculateMedian(list) {
 }
 
 module.exports = calculateMedian;
+
+function calculateMedian(list) {
+  // Sort the list in ascending order
+  list.sort((a, b) => a - b);
+
+  const middleIndex = Math.floor(list.length / 2);
+
+  if (list.length % 2 === 0) {
+    // If the list has an even number of elements, return the average of the two middle values
+    return (list[middleIndex - 1] + list[middleIndex]) / 2;
+  } else {
+    // If the list has an odd number of elements, return the middle element
+    return list[middleIndex];
+  }
+}
+
+module.exports = calculateMedian;

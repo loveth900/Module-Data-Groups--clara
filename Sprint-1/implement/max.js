a27eeadd05bfd49f40b2e897d38b23e26646a1d6
loveth900 <ojay_love@yahoo.com>
2024-12-15 06:48:23 +0000

    the functions was found

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -2,3 +2,8 @@ function findMax(elements) {
 }
 
 module.exports = findMax;
+
+console.log(findMax([1, 2, 3])); // Output: 3
+console.log(findMax([10, 5, 8, 25])); // Output: 25
+console.log(findMax([99, 0, -5])); // Output: 99
+console.log(findMax([])); // Output: undefined

4e7b7b04e4e54aa0ee5fe53cda6354f711be51fe
loveth900 <ojay_love@yahoo.com>
2024-12-15 06:45:23 +0000

    the test of array was implemented

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -10,7 +10,6 @@ E.g. dedupe(['a','a','a','b','b','c']) target output: ['a','b','c']
 E.g. dedupe([5, 1, 1, 2, 3, 2, 5, 8]) target output: [5, 1, 2, 3, 8]
 E.g. dedupe([1, 2, 1]) target output: [1, 2]
 */
-
 // Acceptance Criteria:
 
 // Given an empty array
@@ -25,3 +24,26 @@ test.todo("given an empty array, it returns an empty array");
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
+
+
+const dedupe = require("./dedupe.js");
+
+describe("dedupe", () => {
+
+  test("given an empty array, it returns an empty array", () => {
+    expect(dedupe([])).toEqual([]);
+  });
+
+  test("given an array with no duplicates, it returns a copy of the original array", () => {
+    expect(dedupe([1, 2, 3])).toEqual([1, 2, 3]);
+    expect(dedupe(['a', 'b', 'c'])).toEqual(['a', 'b', 'c']);
+  });
+
+  test("given an array with strings or numbers, it removes the duplicate values, preserving the first occurrence", () => {
+    expect(dedupe(['a', 'a', 'a', 'b', 'b', 'c'])).toEqual(['a', 'b', 'c']);
+    expect(dedupe([5, 1, 1, 2, 3, 2, 5, 8])).toEqual([5, 1, 2, 3, 8]);
+    expect(dedupe([1, 2, 1])).toEqual([1, 2]);
+  });
+
+});
+
